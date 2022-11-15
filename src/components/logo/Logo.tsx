f2a226d2a936813566f5c4dca7ed0500f9917d46
Ashish Malla <ashishmalla45@gmail.com>
2022-11-14 21:35:26 -0700

    add a logo component and styling for it

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,31 @@
+import Tilt from 'react-parallax-tilt'
+import scanner from '../../assets/scanner.png'
+import './Logo.css'
+
+const Logo = () => {
+	return (
+		<div className='ma4 mt'>
+			<Tilt
+				className='logo br4 shadow-2'
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}
+			>
+				<div className='pa3'>
+					<img
+						src={scanner}
+						alt='scanner icon'
+						style={{
+							height: '150px',
+              width: '150px'
+						}}
+					/>
+				</div>
+			</Tilt>
+		</div>
+	)
+}
+
+export default Logo
