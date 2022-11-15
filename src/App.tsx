c6c0f4bc7ef8e98e89127e7f34d7dc15d9fb7172
Ashish Malla <ashishmalla45@gmail.com>
2022-11-14 21:35:51 -0700

    add logo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Navigation from './components/navigation/Navigation'
 import './App.css'
+import Logo from './components/logo/Logo'
 
 const App = () => {
 	return (
 		<div>
 			<Navigation />
+      <Logo />
 		</div>
 	)
 }
