56a26559b9a478542ede924eeaf8a753d1b5c2a1
Ashish Malla <ashishmalla45@gmail.com>
2022-11-15 23:30:50 -0700

    display ranking info

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import Navigation from './components/Navigation/Navigation'
 import './App.css'
 import Logo from './components/Logo/Logo'
 import ImageLinkInput from './components/ImageLinkInput/ImageLinkInput'
+import Rank from './components/Rank/Rank'
 
 const App = () => {
 	return (
 		<div>
 			<Navigation />
       <Logo />
+      <Rank />
       <ImageLinkInput />
 		</div>
 	)
---
+++ src/components/Rank/Rank.tsx
@@ -0,0 +1,17 @@
+const Rank = () => {
+	return (
+		<div
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			<div className='wheat f2 '>Asiisii, your current rank is...</div>
+			<div className="f3">5</div>
+		</div>
+	)
+}
+
+export default Rank
